0c53bddddfb6daaf7fd1c7c7a804b22dfe461062
yifeitai <neversaynever1022@gmail.com>
2022-12-26 20:52:48 -0500

    update url

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { Container, Box, Grid, Stack } from "@mui/material";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import React from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Homepage from "./pages/HomePage";
-import Detail from "./pages/Detail";
+import { Container, Box, Grid, Stack } from '@mui/material';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Homepage from './pages/HomePage';
+import Detail from './pages/detail';
 
 function App() {
-	return (
-		<Box sx={{ my: 0, py: 0 }}>
-			<Router>
-				<Switch>
-					<Route exact path="/">
-						<Homepage />
-					</Route>
-					<Route exact path="/search">
-						<Detail />
-					</Route>
-				</Switch>
-			</Router>
-		</Box>
-	);
+  return (
+    <Box sx={{ my: 0, py: 0 }}>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Homepage />
+          </Route>
+          <Route exact path='/search'>
+            <Detail />
+          </Route>
+        </Switch>
+      </Router>
+    </Box>
+  );
 }
 
 export default App;
